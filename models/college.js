90844f188c8aca291abd79fdf6a9ee64fe8b5ca0
@Ankan <ankanpal255@gmail.com>
2025-06-07 11:47:42 +0530

    update model

--- models/college.js
+++ models/college.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 const collegeSchema = new mongoose.Schema({
   
 university: String,
-  acceptance_rate: Number,
+  acceptance_rate:String,
   Qs_rank: Number,
   country: String,
   program: [String],
-  level: String,
+  level: [String],
   logoUrl: String,
   createdAt: {
     type: Date,
